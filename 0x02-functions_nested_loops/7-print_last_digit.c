e3dab44cb6839668a6d11f97c049c92ea6f26c44
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-19 02:54:04 +0000

    fix in main.h for task 6

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * 7-print_last_digit - Entry point
+ * @num: used for checking
+ * Return: the value of last digit
+ */
+int print_last_digit(int num)
+{
+	int a;
+
+	if (num < 0)
+		n = -n;
+	a = n % 10;
+}

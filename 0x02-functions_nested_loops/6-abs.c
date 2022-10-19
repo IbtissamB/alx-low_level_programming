b5ae95f233e9f30a61a940583393cd0a59149098
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-19 02:41:49 +0000

    0x02 task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *@num: does the checking
+ * Return: abstract value
+ */
+int _abs(int num)
+{
+	if (n >= 0)
+		return (num);
+	else
+		return (-num);
+}

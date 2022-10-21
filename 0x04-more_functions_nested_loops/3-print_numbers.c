fc91df7d0f92c114e8ac7bd8203c5e374df23904
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-21 02:57:49 +0000

    0x04 task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - print for a digit 0 through 9
+  */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+
+	}
+}

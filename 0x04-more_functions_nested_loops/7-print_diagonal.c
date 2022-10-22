2d39cafeb1539ebfe6f92106feacb547b99a6ff2
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-22 02:24:56 +0000

    0x04 task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int count, i;
+
+		for (count = 0; count < n; count++)
+		{
+			for (i = 0; i < n; i++)
+			{
+				if ( i == count)
+					_putchar('\\');
+				else if (i < count)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}

0042392acade72ae210c3978f988513c03e39d30
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-24 23:21:29 +0000

    0x05 task0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of the variables
+ * the pointer points
+ * @a: pointer
+ * @b: pointer
+ */
+void swap_int(int *a, int *b)
+{
+	int holder;
+
+	holder = *a;
+	*a = *b;
+	*b = holder
+}

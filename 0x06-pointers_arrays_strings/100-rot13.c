0cddfdfbee5a3a993095bda749f5ed9a8d020f86
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-28 00:26:19 +0000

    0x06 task 8 updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,18 +4,18 @@
  * rot13 - encode string
  * @str: input string
  *
- * Return: always (0)
+ * Return: str
  */
 
 char *rot13(char *str)
 {
-	int i = 0, j;
-	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char s[52] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j <= 51; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[j] == str[i])
 			{

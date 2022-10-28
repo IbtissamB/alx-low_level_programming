97a7c0a8f2415cfe5c44281124c8e7729903119e
IbtissamB <ibtissam.bahaj@hotmail.com>
2022-10-28 00:35:37 +0000

    0x06 task 8 updated

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,46 @@
 #include "main.h"
 
 /**
- * rot13 - encode string
- * @str: input string
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
  *
- * Return: str
+ * Return: A pointer to the encoded string.
  */
-
 char *rot13(char *str)
 {
-	int i, j;
-	char s[52] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char s1[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int indx1 = 0, indx2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[indx1])
 	{
-		for (j = 0; j < 52; j++)
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			if (s[j] == str[i])
+			if (str[indx1] == alphabet[indx2])
 			{
-				str[i] = s1[j];
+				str[indx1] = rot13key[indx2];
+				break;
 			}
 		}
+
+		indx1++;
+
 	}
 	return (str);
 }
